aff9bc66654f1a0c63ede210f3305f5846387977
suvendu pal <suvendupal@suvendus-MacBook-Air.local>
2025-12-22 01:22:11 +0530

    Java

--- src/Array/FastArr.java
+++ src/Array/FastArr.java
@@ -1,4 +1,11 @@
 package Array;
 
 public class FastArr {
+    public static void main(String args[])
+    {
+        int[] arr = {10,20,40};
+        for(int i=0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
