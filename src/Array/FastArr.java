8ac27db8d3bd6b137346adde73d37c9d35ad2c5f
suvendu pal <suvendupal@suvendus-MacBook-Air.local>
2025-12-22 01:26:05 +0530

    String

--- src/Array/FastArr.java
+++ src/Array/FastArr.java
@@ -6,6 +6,7 @@ public class FastArr {
         int[] arr = {10,20,40};
         for(int i=0; i<arr.length;i++){
             System.out.print(arr[i]+" ");
+
         }
     }
 }
--- src/String/FastString.java
+++ src/String/FastString.java
@@ -1,4 +1,9 @@
 package String;
 
 public class FastString {
+    public static void main(String args[])
+    {
+        String name = "Suvendu";
+        System.out.println(name);
+    }
 }
