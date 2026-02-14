9b99bbf5235b7006798965308b58b94866f50e2a
suvendu288 <suvendupal288@gmail.com>
2026-02-15 01:17:28 +0530

    Implemented Student Roll And Marks Storage

---
+++ src/Array/StudentMarks2D.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.Scanner;
+
+public class StudentMarks2D {
+    public static void main (String[] args){
+        Scanner sc =new Scanner(System.in);
+        int[][] arr =new int[4][2];
+        for(int i=0;i<4; i++) {
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i=0; i<4; i++){
+            for (int j = 0; j < 2; j++) {
+                System.out.println(arr[i][j]+" ");
+
+            }
+            System.out.println();
+
+
+            }
+        }
+
+
+    }
+
