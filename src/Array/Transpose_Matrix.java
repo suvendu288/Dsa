19ba6cf8adc761acc3ea9dce414c8ace813e095b
suvendu288 <suvendupal288@gmail.com>
2026-02-22 20:04:57 +0530

    Add Transpose_Matrix Matrix program in Java

---
+++ src/Array/Transpose_Matrix.java
@@ -0,0 +1,43 @@
+package Array;
+import java.util.Scanner;
+public class Transpose_Matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Input rows and columns
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+        int[][] matrix = new int[rows][cols];
+        // Input matrix elements
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Print original matrix
+        System.out.println("\nOriginal Matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // Print transpose
+        System.out.println("\nTranspose Matrix:");
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                System.out.print(matrix[j][i] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
+
+
