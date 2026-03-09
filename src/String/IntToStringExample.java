9829dc02c84269f818dffe44b40545de91a25622
suvendu288 <suvendupal288@gmail.com>
2026-03-09 23:54:38 +0530

    add method to convert integer to string

---
+++ src/String/IntToStringExample.java
@@ -0,0 +1,11 @@
+package String;
+
+public class IntToStringExample {
+    public static void main(String[] args) {
+        int number = 456;
+
+        String result = String.valueOf(number);
+
+        System.out.println("Converted String: " + result);
+    }
+}
