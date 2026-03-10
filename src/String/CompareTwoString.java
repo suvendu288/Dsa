9cc973c66b28b39271ed049625cd753f3f58eed3
suvendu288 <suvendupal288@gmail.com>
2026-03-10 19:47:28 +0530

    Java example for comparing two strings using compareTo()

---
+++ src/String/CompareTwoString.java
@@ -0,0 +1,11 @@
+package String;
+
+public class CompareTwoString {
+    public static void main(String[]args){
+        String a="hello";
+        String b="world";
+        System.out.println(a.compareTo(b));
+
+
+    }
+}
