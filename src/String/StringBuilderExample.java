80b54883b01d0536765ead2a23fefec732b8c2fc
suvendu288 <suvendupal288@gmail.com>
2026-03-15 22:41:59 +0530

    StringBuilder basic example 3️

---
+++ src/String/StringBuilderExample.java
@@ -0,0 +1,17 @@
+package String;
+
+public class StringBuilderExample {
+    public static void main(String[] args) {
+
+        // Create a StringBuilder object
+        StringBuilder sb = new StringBuilder();
+
+        // Append strings
+        sb.append("Hello");
+        sb.append(" ");
+        sb.append("World");
+
+        // Print result
+        System.out.println("Result: " + sb.toString());
+    }
+}
