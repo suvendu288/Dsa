6d04802fdf9e8a6e776b5329faae93d7a80454be
suvendu288 <suvendupal288@gmail.com>
2026-03-22 00:16:24 +0530

    implement string compression logic
    3️

---
+++ src/String/InsexandCompare.java
@@ -0,0 +1,10 @@
+package String;
+
+public class InsexandCompare {
+    public static void main(String[]args){
+        String str="suvendu pal";
+        System.out.println(str.indexOf('u'));
+
+
+    }
+}
---
+++ src/String/whyString.java
@@ -0,0 +1,13 @@
+package String;
+
+public class whyString {
+    public static void main(String[]args){
+       // char[] name={'s','u','v','e','n','d','u'};
+        //for(char ele: name){
+            //System.out.print(ele);
+       // }
+        String name="suvendu";
+        System.out.println(name);
+
+    }
+}
